3f316c70c50ca0fe60373852ee683e9ea13aa514
Matt Dear <mrmdear@gmail.com>
2021-10-25 14:01:57 +0100

    Updated the custom install functions and tested it on machine.

--- macbookApplicationInstall.sh
+++ macbookApplicationInstall.sh
@@ -1,21 +1,24 @@
 #!/bin/zsh
 
-# Custom apt install function
+# Custom brew install function
 brewinstall(){
   echo "Installing $1 \c"
   brew install $2 1>/dev/null 2>/tmp/stderr && echo 'Complete' || echo -e 'Error: \c' && cat /tmp/stderr | egrep '^E: ' | sed 's/^E: //'
 }
 
+# Custom cask install function
+caskinstall(){
+  echo "Installing $1 \c"
+  brew install --cask $2 1>/dev/null 2>/tmp/stderr && echo 'Complete' || echo -e 'Error: \c' && cat /tmp/stderr | egrep '^E: ' | sed 's/^E: //'
+}
+
 # Script start
 clear
-echo "#################### Macbook Application Installer Started #####################"
-
-# Install Brew
--c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+echo "##################### Mac OS Application Installer Started #####################"
 
 # Brew installs
 echo " "
-echo "                                  Brew installs                                 "
+echo "                                  Brew Installs                                 "
 brewinstall 'Vim ........................................................' vim
 brewinstall 'Pipenv .....................................................' pipenv
 brewinstall 'Python 3.8 .................................................' python@3.8
@@ -23,17 +26,16 @@ brewinstall 'Docker .....................................................' docke
 
 # Cask installs
 echo " "
-echo "                                  Cask installs                                 "
-caskinstall 'Intellij Idea CE ...........................................' --cask intellij-idea-ce
-caskinstall 'Pycharm CE .................................................' --cask pycharm-ce
-caskinstall 'Visual Studio Code .........................................' --cask visual-studio-code
-caskinstall 'Open JDK 11 ................................................' --cask adoptopenjdk@11
-caskinstall 'Spotify ....................................................' --cask spotify
-caskinstall 'Signal .....................................................' --cask signal
-caskinstall 'Google Chrome ..............................................' --cask google-chrome
-caskinstall 'Draw IO ....................................................' --cask drawio
-caskinstall 'Discord ....................................................' --cask discord
+echo "                                  Cask Installs                                 "
+caskinstall 'Intellij Idea CE ...........................................' intellij-idea-ce
+caskinstall 'Pycharm CE .................................................' pycharm-ce
+caskinstall 'Visual Studio Code .........................................' visual-studio-code
+caskinstall 'Open JDK 11 ................................................' adoptopenjdk@11
+caskinstall 'Spotify ....................................................' spotify
+caskinstall 'Signal .....................................................' signal
+caskinstall 'Google Chrome ..............................................' google-chrome
+caskinstall 'Discord ....................................................' discord
 
 # Script end
 echo " "
-echo "#################### Macbook Application Installer Complete ####################"
+echo "##################### Mac OS Application Installer Complete ####################"
